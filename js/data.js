f17827eec8cb5f5b4231c0b3ffbc750f6eb562ab
sora-web <brave27yuto@gmail.com>
2022-10-05 18:06:17 +0900

    表示　title/category/tags

---
+++ js/data.js
@@ -0,0 +1,225 @@
+const categories = [
+  {
+    id: 'category_1',
+    title: 'category_aaa',
+  },
+  {
+    id: 'category_2',
+    title: 'category_bbb',
+  },
+  {
+    id: 'category_3',
+    title: 'category_ccc',
+  },
+  {
+    id: 'category_4',
+    title: 'category_ddd',
+  },
+  {
+    id: 'category_5',
+    title: 'category_eee',
+  },
+]
+
+const tags = [
+  {
+    id: 'tag_1',
+    title: 'tag_aaa',
+  },
+  {
+    id: 'tag_2',
+    title: 'tag_bbb',
+  },
+  {
+    id: 'tag_3',
+    title: 'tag_ccc',
+  },
+  {
+    id: 'tag_4',
+    title: 'tag_ddd',
+  },
+  {
+    id: 'tag_5',
+    title: 'tag_eee',
+  },
+  {
+    id: 'tag_6',
+    title: 'tag_fff',
+  },
+  {
+    id: 'tag_7',
+    title: 'tag_ggg',
+  },
+  {
+    id: 'tag_8',
+    title: 'tag_hhh',
+  },
+  {
+    id: 'tag_9',
+    title: 'tag_iii',
+  },
+  {
+    id: 'tag_10',
+    title: 'tag_jjj',
+  },
+]
+
+
+const posts = [
+  {
+    id: 'post_1',
+    title: 'post_aaa',
+    category_id: 'category_1',
+    tag_ids: ['tag_1', 'tag_2', 'tag_3']
+  },
+  {
+    id: 'post_2',
+    title: 'post_bbb',
+    category_id: 'category_3',
+    tag_ids: ['tag_9']
+  },
+  {
+    id: 'post_3',
+    title: 'post_ccc',
+    category_id: 'category_4',
+    tag_ids: ['tag_5', 'tag_6']
+  },
+  {
+    id: 'post_4',
+    title: 'post_ddd',
+    category_id: 'category_2',
+    tag_ids: ['tag_7', 'tag_8', 'tag_10']
+  },
+  {
+    id: 'post_5',
+    title: 'post_eee',
+    category_id: 'category_5',
+    tag_ids: ['tag_1', 'tag_7']
+  },
+  {
+    id: 'post_6',
+    title: 'post_fff',
+    category_id: 'category_5',
+    tag_ids: ['tag_1', 'tag_2', 'tag_3', 'tag_8', 'tag_10']
+  },
+  {
+    id: 'post_7',
+    title: 'post_ggg',
+    category_id: 'category_4',
+    tag_ids: ['tag_1', 'tag_3']
+  },
+  {
+    id: 'post_8',
+    title: 'post_hhh',
+    category_id: 'category_1',
+    tag_ids: []
+  },
+  {
+    id: 'post_9',
+    title: 'post_iii',
+    category_id: 'category_2',
+    tag_ids: ['tag_2', 'tag_8']
+  },
+  {
+    id: 'post_10',
+    title: 'post_jjj',
+    category_id: 'category_3',
+    tag_ids: ['tag_5', 'tag_6', 'tag_7']
+  },
+  {
+    id: 'post_11',
+    title: 'post_kkk',
+    category_id: 'category_1',
+    tag_ids: ['tag_4', 'tag_5', 'tag_6', 'tag_8']
+  },
+  {
+    id: 'post_12',
+    title: 'post_lll',
+    category_id: 'category_4',
+    tag_ids: ['tag_3']
+  },
+  {
+    id: 'post_13',
+    title: 'post_mmm',
+    category_id: 'category_3',
+    tag_ids: ['tag_8', 'tag_9', 'tag_10']
+  },
+  {
+    id: 'post_14',
+    title: 'post_nnn',
+    category_id: 'category_2',
+    tag_ids: []
+  },
+  {
+    id: 'post_15',
+    title: 'post_ooo',
+    category_id: 'category_5',
+    tag_ids: ['tag_2', 'tag_4', 'tag_6']
+  },
+  {
+    id: 'post_16',
+    title: 'post_ppp',
+    category_id: 'category_3',
+    tag_ids: ['tag_1', 'tag_3', 'tag_5', 'tag_7', 'tag_9']
+  },
+  {
+    id: 'post_17',
+    title: 'post_qqq',
+    category_id: 'category_1',
+    tag_ids: ['tag_1', 'tag_10']
+  },
+  {
+    id: 'post_18',
+    title: 'post_rrr',
+    category_id: 'category_2',
+    tag_ids: ['tag_3', 'tag_6', 'tag_9']
+  },
+  {
+    id: 'post_19',
+    title: 'post_sss',
+    category_id: 'category_5',
+    tag_ids: ['tag_4', 'tag_7', 'tag_10']
+  },
+  {
+    id: 'post_20',
+    title: 'post_ttt',
+    category_id: 'category_3',
+    tag_ids: ['tag_8']
+  },
+  {
+    id: 'post_21',
+    title: 'post_uuu',
+    category_id: 'category_2',
+    tag_ids: ['tag_1', 'tag_2', 'tag_3', 'tag_8', 'tag_9', 'tag_10']
+  },
+  {
+    id: 'post_22',
+    title: 'post_vvv',
+    category_id: 'category_4',
+    tag_ids: ['tag_4', 'tag_5', 'tag_6', 'tag_7']
+  },
+  {
+    id: 'post_23',
+    title: 'post_www',
+    category_id: 'category_5',
+    tag_ids: []
+  },
+  {
+    id: 'post_24',
+    title: 'post_xxx',
+    category_id: 'category_2',
+    tag_ids: ['tag_1']
+  },
+  {
+    id: 'post_25',
+    title: 'post_yyy',
+    category_id: 'category_1',
+    tag_ids: ['tag_1', 'tag_5']
+  },
+  {
+    id: 'post_26',
+    title: 'post_zzz',
+    category_id: 'category_4',
+    tag_ids: ['tag_1', 'tag_4', 'tag_7', 'tag_10']
+  }
+];
--- js/script.js
+++ js/script.js
@@ -1,80 +1,52 @@
-//burger
-$(".p-header__burger").on("click", function () {
-  $("body").toggleClass("is__fixed");
-  $(".p-header__burger").toggleClass("is__active");
-  $(".p-burger-view").toggleClass("is__open");
-});
+//タイトル出力
+var titleList = "";
+for (var i = 0; i < posts.length; i++) {
+  titleList += "<li>" + posts[i].title + "</li>";
+}
+document.getElementById("title").innerHTML = titleList;
 
-//sticky header
-$(function () {
-  // ページ下部固定ボタン表示非表示
-  $(window).on("load scroll", function () {
-    $header = $(".p-header");
-    btnOffset = $(".l-cont--contact").offset().top;
-    x = $(".p-header").height();
-    fixedClass = "p-header--fixed";
-  });
-  $(function () {
-    $(window).scroll(function () {
-      if ($(this).scrollTop() > btnOffset - x) {
-        $header.addClass(fixedClass);
-      } else {
-        $header.removeClass(fixedClass);
+//カテゴリー出力
+var categoryList = "";
+for (var i = 0; i < posts.length; i++) {
+  categoryId = posts[i].category_id;
+  for (var category_i = 0; category_i < categories.length; category_i++) {
+    if (categoryId === categories[category_i].id) {
+      categoryList += "<li>" + categories[category_i].title + "</li>";
+    }
+  }
+}
+document.getElementById("category").innerHTML = categoryList;
+
+//
+//
+//tagタイトル用のulを作成
+var tagsUlTitle = "";
+for (var tagUl_i = 0; tagUl_i < posts.length; tagUl_i++) {
+  tagsUlTitle +=
+    "<li>" +
+    "<ul  id =" +
+    "tag_title" +
+    [tagUl_i] +
+    ">" +
+    "aaa" +
+    "</ul>" +
+    "</li>";
+}
+document.getElementById("tags_title").innerHTML = tagsUlTitle;
+
+//ulタイトルの中身
+for (var i = 0; i < posts.length; i++) {
+  var tagsList = "";
+  for (var tags_i = 0; tags_i < posts[i].tag_ids.length; tags_i++) {
+    for (var tag_i = 0; tag_i < tags.length; tag_i++) {
+      if (posts[i].tag_ids[tags_i] === tags[tag_i].id) {
+        tagsList += "<li>" + tags[tag_i].title + "</li>";
       }
-    });
-  });
-});
-
-// swiper
-const swiper = new Swiper(".swiper", {
-  direction: "horizontal",
-  offsetPxBefore: 1000,
-  centeredSlides: true,
-  loop: true,
-
-  watchSlidesVisibility: true,
-  speed: 1000,
-  slidesPerView: 1.2,
-  // spaceBetween: 20,
-
-  autoplay: {
-    delay: 5000,
-  },
-  breakpoints: {
-    640: {
-      autoplay: false,
-      slidesPerView: 2.2,
-      // spaceBetween: 50,
-    },
-    960: {
-      autoplay: false,
-      slidesPerView: 2.3,
-      // spaceBetween: 50,
-    },
-  },
-  pagination: {
-    el: ".js_swiper_pagination",
-  },
-  navigation: {
-    nextEl: ".js_swiper-next",
-    prevEl: ".js_swiper-prev",
-  },
-});
-
-//MV浮遊
-function randomValues() {
-  anime({
-    targets: ".p-mv-round",
-    translateX: function () {
-      return anime.random(-400, 400);
-    },
-    translateY: function () {
-      return anime.random(-400, 400);
-    },
-    easing: "easeInOutQuad",
-    duration: 20000,
-    complete: randomValues,
-  });
+    }
+  }
+  document.getElementById("tag_title" + [i]).innerHTML = tagsList;
 }
 
-randomValues();
+document.querySelectorAll(".c-table__list li").forEach(function (list) {
+  list.classList.add("lg-mb10");
+});
