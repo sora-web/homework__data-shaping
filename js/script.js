c10671bccf72b21232f7fbe61cbcfb42f09e4800
sora-web <brave27yuto@gmail.com>
2022-10-06 11:56:34 +0900

    列に関連性あり

--- js/script.js
+++ js/script.js
@@ -1,52 +1,90 @@
-//タイトル出力
-var titleList = "";
 for (var i = 0; i < posts.length; i++) {
-  titleList += "<li>" + posts[i].title + "</li>";
-}
-document.getElementById("title").innerHTML = titleList;
+  //
+  //
+  //wrapper作る
+  const tbody = document.getElementById("tbody");
+  let tr_element = document.createElement("tr");
 
-//カテゴリー出力
-var categoryList = "";
-for (var i = 0; i < posts.length; i++) {
-  categoryId = posts[i].category_id;
+  tbody.appendChild(tr_element);
+
+  //
+  //タイトルゾーン
+  //
+
+  //
+  //タイトル用のtdを作る
+  let td_title = document.createElement("td");
+  td_title.classList.add("c-table__data");
+  let id_title = "title" + [i];
+  td_title.setAttribute("id", id_title);
+  tr_element.appendChild(td_title);
+
+  //
+  //タイトルを出力
+  document.getElementById(id_title).innerHTML = posts[i].title;
+
+  //
+  //カテゴリーゾーン
+  //
+
+  //
+  //カテゴリー用のtdを作る
+  let td_category = document.createElement("td");
+  td_category.classList.add("c-table__data");
+  let id_category = "cateobory" + [i];
+  td_category.setAttribute("id", id_category);
+  tr_element.appendChild(td_category);
+
+  //
+  //postsのcategory_idをcategoriesのtitleに変換
+  //
+  //カテゴリーを出力
   for (var category_i = 0; category_i < categories.length; category_i++) {
-    if (categoryId === categories[category_i].id) {
-      categoryList += "<li>" + categories[category_i].title + "</li>";
+    if (posts[i].category_id === categories[category_i].id) {
+      document.getElementById(id_category).innerHTML =
+        categories[category_i].title;
     }
   }
-}
-document.getElementById("category").innerHTML = categoryList;
 
-//
-//
-//tagタイトル用のulを作成
-var tagsUlTitle = "";
-for (var tagUl_i = 0; tagUl_i < posts.length; tagUl_i++) {
-  tagsUlTitle +=
-    "<li>" +
-    "<ul  id =" +
-    "tag_title" +
-    [tagUl_i] +
-    ">" +
-    "aaa" +
-    "</ul>" +
-    "</li>";
-}
-document.getElementById("tags_title").innerHTML = tagsUlTitle;
+  //
+  //タグゾーン
+  //
 
-//ulタイトルの中身
-for (var i = 0; i < posts.length; i++) {
-  var tagsList = "";
-  for (var tags_i = 0; tags_i < posts[i].tag_ids.length; tags_i++) {
-    for (var tag_i = 0; tag_i < tags.length; tag_i++) {
-      if (posts[i].tag_ids[tags_i] === tags[tag_i].id) {
-        tagsList += "<li>" + tags[tag_i].title + "</li>";
+  if (posts[i].tag_ids.length) {
+    //
+    //タグ用のtdを作る
+    let td_tags = document.createElement("td");
+    td_tags.classList.add("c-table__data");
+    let id_tags = "tags" + [i];
+    td_tags.setAttribute("id", id_tags);
+    tr_element.appendChild(td_tags);
+
+    //
+    //タグ用のulを作る
+    let tags_list = document.createElement("ul");
+    tags_list.classList.add("c-table__list");
+    td_tags.appendChild(tags_list);
+
+    //
+    //タグ用のliを作る
+    //
+    //tag_iはpostsのtag＿idsのカウント用
+    for (var tag_i = 0; tag_i < posts[i].tag_ids.length; tag_i++) {
+      let tags_item = document.createElement("li");
+      tags_item.classList.add("c-table__item");
+      let id_tag = "tag" + [i] + "__" + [tag_i];
+      tags_item.setAttribute("id", id_tag);
+      tags_list.appendChild(tags_item);
+
+      //
+      //タグを出力
+      //
+      //tags_iはtagsのidのカウント用
+      for (var tags_i = 0; tags_i < tags.length; tags_i++) {
+        if (posts[i].tag_ids[tag_i] === tags[tags_i].id) {
+          document.getElementById(id_tag).innerHTML = tags[tags_i].title;
+        }
       }
     }
   }
-  document.getElementById("tag_title" + [i]).innerHTML = tagsList;
 }
-
-document.querySelectorAll(".c-table__list li").forEach(function (list) {
-  list.classList.add("lg-mb10");
-});
